9b1986356f6ff63e660c2e53be0f9d4bfcac508d
infoharry99 <infoharry99@gmail.com>
2025-02-19 20:10:53 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,11 @@ import AnexVII from "./components/anexForm/AnnexVII";
 import AnexCreate from "./components/anexForm/AnexFormcreate";
 import Otp from "./components/auth/login/otp";
 import OtpVerify from "./components/auth/login/otpverify";
-import CreateCompany from "./components/auth/createCompany/CreateCompany";
-import Test from "./components/auth/createCompany/Test";
-import Form from "./components/anexForm/Form";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/company" element={<Company />} />
@@ -26,10 +22,6 @@ const App = () => {
         <Route path="/otp" element={<Otp />} />
         <Route path="/otpverify" element={<OtpVerify />} />
         
-=======
-        <Route path="/*" element={<Login />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
->>>>>>> a2310d1e78e03fc31c3db4997b0b5c3b03be727a
       </Routes>
     </Router>
   );
