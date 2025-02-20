e84056745a8a1314657e1a3c3595d80112aefbed
Navdeep Pathan <151203532+navdeeppathan@users.noreply.github.com>
2025-02-20 23:23:01 +0530

    pdf commit3

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/auth/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import Company from "./components/company/Companies";
-
 import AnexCreate from "./components/anexForm/AnexFormcreate";
 import Otp from "./components/auth/login/otp";
 import OtpVerify from "./components/auth/login/otpverify";
 import AdminDashboard from "./components/Admin/dashboard/AdminDashboard";
-import PDFMaker from "./components/pdfMaker/PDFMaker";
-import ShipmentDetails from "./components/pdfMaker/PDFMakerOrgnl";
+import PDFMakerOrgnl from "./components/pdfMaker/PDFMakerOrgnl";
 
 const App = () => {
   return (
@@ -22,7 +20,7 @@ const App = () => {
         <Route path="/anexcreate" element={<AnexCreate />} />
         <Route path="/otp" element={<Otp />} />
         <Route path="/otpverify" element={<OtpVerify />} />
-        <Route path="/pdf" element={<ShipmentDetails />} />
+        <Route path="/pdf" element={<PDFMakerOrgnl />} />
       </Routes>
     </Router>
   );
